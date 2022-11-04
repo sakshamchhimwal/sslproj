9f983bf4b0dbb895ba8cc03aceefbdb6b139610a
sakshamchhimwal <sakshamchhimwal2410@gmail.com>
2022-11-05 00:25:45 +0530

    checkcommit

--- User.class.php
+++ User.class.php
@@ -3,7 +3,7 @@
  * User Class 
  * This class is used for database related (connect, insert, and update) operations 
  * @author    CodexWorld.com 
- * @url        http://www.codexworld.com 
+ * @url        http://www.codexworld.co
  * @license    http://www.codexworld.com/license 
  */ 
  
