78b1a31719afd4ee5f84d43e0a072360daa9d600
sakshamchhimwal <sakshamchhimwal2410@gmail.com>
2022-11-07 00:40:58 +0530

    addnewgist workin

--- codespace.php
+++ codespace.php
@@ -10,13 +10,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CodeSpace</title>
+    <script>
+      function makeCooki(){
+    let code = document.getElementById("inputArea")["value"];
+    code = code.split("\n").join("\\join");
+    document.cookie = "xcode = "+code;
+    console.log(document.cookie);}
+
+  </script>
 </head>
 <body>
     <form action="insertgist.php" method="post">
       <pre>
         <textarea cols=80 rows=30 id="inputArea" name="code"></textarea>
     </pre>
-      <button type="submit">Save As Gist</button>
+      <button type="submit" id="subbut" onClick=makeCooki()>Save As Gist</button>
       <pre>
         <div class="outputBox">
         </div>
@@ -24,31 +32,14 @@
     </form>
 </body>
 <script>
-  
-// Creating a cookie after the document is ready
-let x=doucment.getElementById("inputArea").innerHTML;
-x=x.split('\n');
-console.log((x));
-$(document).ready(function () {
-    createCookie("gfg", "GeeksforGeeks", "10");
-});
-   
-// Function to create the cookie
-function createCookie(name, value, days) {
-    var expires;
-      
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toGMTString();
-    }
-    else {
-        expires = "";
-    }
-      
-    document.cookie = escape(name) + "=" + 
-        escape(value) + expires + "; path=/";
-}
-  
+  document.addEventListener("keydown", keyPressed);
+function keyPressed(e) {
+  if (e["key"] === "Enter") {
+    let code = document.getElementById("inputArea")["value"];
+    code = code.split("\n").join("\\join");
+    document.cookie = "xcode = "+code;
+    console.log(document.cookie);
+  }
+}  
 </script>
 </html>
